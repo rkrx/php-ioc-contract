870caf4bdf99d71f92ff7c9aebfe8ba54608ba10
ron <ron.kirschler@gmail.com>
2014-11-11 21:47:59 +0100

    Initial import

--- src/MethodInvoker.php
+++ src/MethodInvoker.php
@@ -6,7 +6,7 @@ interface MethodInvoker {
 	 * Invokes the callable $callable with the arguments $arguments. If arguments were missing in $arguments, the
 	 * method-invoker tries to fill the missing arguments by itself.
 	 *
-	 * @param $callable
+	 * @param callable $callable
 	 * @param array $arguments
 	 * @return mixed
 	 */
