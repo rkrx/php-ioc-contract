faee81eebcb15916d3b1853918a9c305e96e1c5f
rkrx <ron.kirschler@gmail.com>
2014-11-14 11:31:40 +0100

    Update MethodInvoker.php

--- src/MethodInvoker.php
+++ src/MethodInvoker.php
@@ -11,6 +11,21 @@ interface MethodInvoker {
 	 * method or a class not yet instantiated. The implementation determines how to handle the callable and find the
 	 * appropriate method for invocation. If no matching class could be found, it must throw an
 	 * Ioc\Exceptions\DefinitionNotFoundException.
+	 * 
+	 * Following forms must be accepted:
+	 * 
+	 * $methodInvoker->invoke(function () {}, $arguments);
+	 * A closure
+	 * 
+	 * $methodInvoker->invoke(array($instance, 'methodName'), $arguments);
+	 * A classic callable with an already instantiated $instance and a methodName.
+	 * 
+	 * $methodInvoker->invoke(array('Full\\Qualified\\Class\\Name', 'methodName'), $arguments);
+	 * This is not(!) a static call. Full\Qualified\Class\Name gats instantiated first before calling methodName.
+	 * 
+	 * The called method could have either scalar parameters (with or without default values) or Typehinted patemeters
+	 * mixed together. Scalar-types and array-types can not be determined automatically. They must be provided through
+	 * $arguments.
 	 *
 	 * @param callable $callable
 	 * @param array $arguments Must be an array were the keys match to the Variable-Names of the __construct'ors parameters.
